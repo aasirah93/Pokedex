8770abdf39330ce5ddfffa025f880f584ac15d5f
aasirah93 <62110929+aasirah93@users.noreply.github.com>
2020-08-07 19:31:30 +0100

    creating state in app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PokemonList from './components/PokemonList';
+
+class App extends Component {
+  render() {
+    return (
+     <div> 
+       <PokemonList />
+     </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/PokemonList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PokemonList extends Component {
+  render() {
+    return (
+     <div> 
+       <h1>Bulbasaur, Charmander</h1>
+     </div>
+    );
+  }
+}
+
+export default PokemonList;
