49c83abe76d05296850344cf4d9a7d8cccf0b61f
aasirah93 <62110929+aasirah93@users.noreply.github.com>
2020-08-20 20:35:33 +0100

    axios requests and useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PokemonList from './components/PokemonList.js'
+import axios from 'axios'
 
 function App()  {
-  const [pokemon, setPokemon] = useState(["bulbasaur, charmander"])
+  const [pokemon, setPokemon] = useState([])
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
   
+useEffect(() => {
+  
+  axios.get(currentPageUrl).then(res => {
+    setPokemon(res.data.results.map(p => p.name))
+  })
+}, [currentPageUrl])
+
   return (
     <PokemonList pokemon={pokemon}/>
   );
